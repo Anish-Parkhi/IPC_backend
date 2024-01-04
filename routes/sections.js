f9ffc0bc3470c13df4eefbf625ba168c25ff294d
Anish-Parkhi <anishparkhi03@gmail.com>
2024-01-04 23:16:38 +0530

    added route for sections by number

--- routes/sections.js
+++ routes/sections.js
@@ -12,34 +12,24 @@ router.get('/sections', async (req, res) => {
   }
 });
 
-router.get('/sections/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const section = await sectionNameModel.findById(id);
-    res.status(200).json(section);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-});
+// router.post('/sectionsAdd', async (req, res) => {
+//   const { chapter, chapter_title, Section, section_desc, section_title } =
+//     req.body;
 
-router.post('/sectionsAdd', async (req, res) => {
-  const { chapter, chapter_title, Section, section_desc, section_title } =
-    req.body;
+//   const newChapter = new sectionNameModel({
+//     chapter,
+//     chapter_title,
+//     Section,
+//     section_desc,
+//     section_title,
+//   });
 
-  const newChapter = new sectionNameModel({
-    chapter,
-    chapter_title,
-    Section,
-    section_desc,
-    section_title,
-  });
-
-  try {
-    await newChapter.save();
-    res.status(201).json({ message: 'Chapter added successfully' });
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-});
+//   try {
+//     await newChapter.save();
+//     res.status(201).json({ message: 'Chapter added successfully' });
+//   } catch (error) {
+//     res.status(409).json({ message: error.message });
+//   }
+// });
 
 export default router;
